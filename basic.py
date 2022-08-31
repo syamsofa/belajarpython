c149c61c801271c90ba739098c3994c2834e13b2
achiruzaman <achiruzaman@gmail.com>
2022-08-31 14:38:46 +0700

    UPDATE BASIC

--- basic.py
+++ basic.py
@@ -11,7 +11,7 @@ x=input("something:")
 print(x)
 
 # raise exceprion
-raise IOError("File error")
+# raise IOError("File error")
 
 # argument in exception
 # except Myerror as err:
@@ -19,3 +19,9 @@ raise IOError("File error")
 # next() function
 gen=(letter for letter in 'HELOWORLD')
 next(gen)
+
+def area(x,y=3.14): #Fo
+    return "ok"
+
+print(area(1,1))
+
