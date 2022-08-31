925b7aa2eb4762e4e44fff2981b26f79a99e7238
achiruzaman <achiruzaman@gmail.com>
2022-08-31 13:55:42 +0700

    UPDATE BASIC

---
+++ basic.py
@@ -0,0 +1,21 @@
+from __future__ import division
+k= "nama saya"
+l="heruzaman"
+
+# print
+print("OK")
+print(k,end=" ")
+
+#Reading from keyboared
+x=input("something:")
+print(x)
+
+# raise exceprion
+raise IOError("File error")
+
+# argument in exception
+# except Myerror as err:
+
+# next() function
+gen=(letter for letter in 'HELOWORLD')
+next(gen)
